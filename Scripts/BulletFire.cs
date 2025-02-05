7aaef17125879383e7cc7a84af44544b0d691db6
Oberonru <oberonru@mail.ru>
2025-02-05 19:35:44 +0600

    Create bullet, bullet fire

---
+++ Scripts/BulletFire.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletForce = 1f;
+    [SerializeField] private AudioSource fire;
+    private float _gameTime;
+
+    private void Update()
+    {
+         _gameTime += Time.deltaTime;
+        
+        if (Input.GetMouseButton(0) && _gameTime > 0.15f)
+        {
+            Schoot();
+            _gameTime = 0;
+        }
+    }
+
+    private void Schoot()
+    {
+       GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+       bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletForce;
+       fire.pitch = Random.Range(0.8f, 1.2f);
+       fire.Play();
+    }
+}
